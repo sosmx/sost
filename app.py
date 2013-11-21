8524de93ea32f7b031adab3b3f182e02023f9f71
Soulou <leo@unbekandt.eu>
2013-11-21 10:55:18 +0000

    Add link to source code

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
